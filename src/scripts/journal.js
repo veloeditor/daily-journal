395f428bc0b5d7af36a11bc6b26677fe8a9dc96b
Brian Wilson <i9_editor@Brians-MacBook-Pro-2.local>
2019-07-16 19:22:20 -0500

    July 16 entry

---
+++ src/scripts/journal.js
@@ -0,0 +1,38 @@
+const journalEntry = {
+     date: "08.12.2019",
+     concepts: "Flexbox, gitHub, teamwork",
+     entry: "This week we learned Flexbox, using gitHub wiht a team, built a celebrity tribute project with a team and started in to JavaScript with manipulating the DOM and intro to objects",
+     mood: "Happy"  
+}
+
+const journalArray = [];
+
+console.log(journalArray)
+
+const journalEntry2 = {
+    date: "08.12.2019",
+    concepts: "Javascript arrays, functions",
+    entry: "I have some confidence but I know I have a weakness of reading comprehension. I'm worried that this will cause me to fall behind. I need to slow down and fully read out what is being asked of me.",
+    mood: "Apprehensive"  
+}
+
+const journalEntry3 = {
+    date: "08.15.2019",
+    concepts: "JS objects",
+    entry: "We just finished our 2nd day of JS and my brain hurts a bit. I was able to nail the first exercise which was the ChickenMonkey exercise. The additional exercises were harder for me to initial figure out, but began to sink in to learning tmeplate literals which are the way to go. I feel I'm grasping about 90% of things so far but feel uneasy about the coming ays",
+    mood: "Apprehensive"  
+}
+
+const journalEntry4 = {
+    date: "08.16.2019",
+    concepts: "JS functions and more objects",
+    entry: "Struggled with today's exercises but figured out answers. I worked a lot with Bryan today to work through some issues. I have a slight sense of confidence with being able to figure out the first two sets of exercises on TryIt",
+    mood: "Happy"  
+}
+
+journalArray.push(journalEntry);
+journalArray.push(journalEntry2)
+journalArray.push(journalEntry3)
+journalArray.push(journalEntry4)
+
+console.log(journalArray)
