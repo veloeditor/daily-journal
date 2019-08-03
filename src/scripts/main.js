df581cffd2ae89db13f1791d9a38e1b656b99c60
Brian Wilson <wilsonedit@gmail.com>
2019-08-03 14:08:34 -0500

    Update main.js
    added character limit validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,37 +10,34 @@ const journalDateField = document.querySelector("#journalDate")
 const conceptsCoveredField = document.querySelector("#conceptsCovered")
 const moodSelect = document.querySelector("#mood_select")
 const journalEntryField = document.querySelector("#journalEntry")
+const maxLength = document.querySelector("#journalEntry").maxLength;
 
 submitEntry.addEventListener("click", event => {
     
-            
-            const dateValue = journalDateField.value
+        const dateValue = journalDateField.value
+        const conceptsValue = conceptsCoveredField.value
+        const entryValue = journalEntryField.value
+            if (entryValue.length <= 200) {
+    
+            } else {
+                return alert("Please limit the number of characters to under 200")
+            }
+        const moodValue = moodSelect.value
+        
+        //use values from input fields to create the entry:
 
-            
-            const conceptsValue = conceptsCoveredField.value
+        const newJournalEntry = journalEntryObject(dateValue, conceptsValue, entryValue, moodValue)
 
-            
-            const entryValue = journalEntryField.value
-
-            
-            const moodValue = moodSelect.value
-
-            //need input validation here -- STILL NEED TO DO
-
-            //use values from input fields to create the entry:
-
-            const newJournalEntry = journalEntryObject(dateValue, conceptsValue, entryValue, moodValue)
-
-            saveJournalEntry(newJournalEntry)
-                .then(() => {
-                    getAPIData().then(journals => {
-                        console.log('journals: ', journals);
-                        for (const journal of journals) {
-                            const entryHTML = makeJournalEntryComponent(journal.date, journal.concepts, journal.entry, journal.mood);
-                            renderEntry(entryHTML)
-                        }
-                    })
+        saveJournalEntry(newJournalEntry)
+            .then(() => {
+                getAPIData().then(journals => {
+                    console.log('journals: ', journals);
+                    for (const journal of journals) {
+                        const entryHTML = makeJournalEntryComponent(journal.date, journal.concepts, journal.entry, journal.mood);
+                        renderEntry(entryHTML)
+                    }
                 })
+            })
 })
 
 const clearButton = document.querySelector(".clearButton")
